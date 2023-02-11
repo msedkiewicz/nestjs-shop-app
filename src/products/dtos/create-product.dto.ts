ba1cf7218fdd69f2f29704f32c8930aeec83e916
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-11 10:52:33 +0100

    Add validation for product DTO

--- src/products/dtos/create-product.dto.ts
+++ src/products/dtos/create-product.dto.ts
@@ -1,5 +1,19 @@
+import { Transform } from 'class-transformer';
+import { IsInt, IsNotEmpty, IsString, Length, Min } from 'class-validator';
+
 export class CreateProductDTO {
+  @IsNotEmpty()
+  @IsString()
+  @Length(10, 20)
   name: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(0)
   price: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => (Array.isArray(value) ? value.join(', ') : ''))
   description: string;
 }
