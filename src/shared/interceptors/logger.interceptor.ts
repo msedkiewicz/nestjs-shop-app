934c69556eb4feca3a5e26575795b02f350861dc
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-11 11:33:24 +0100

    Modify interceptor

--- src/shared/interceptors/logger.interceptor.ts
+++ src/shared/interceptors/logger.interceptor.ts
@@ -10,7 +10,10 @@ import { tap } from 'rxjs/operators';
 @Injectable()
 export class LoggerInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('***');
+    const url = context.getArgs()[0].url;
+    const method = context.getArgs()[0].method;
+    console.log('================================');
+    console.log(`${method} ${url} `);
     console.log(`Start request in ${context.getClass().name}`);
 
     const start = Date.now();
@@ -18,7 +21,7 @@ export class LoggerInterceptor implements NestInterceptor {
     return next.handle().pipe(
       tap(() => {
         console.log(`Request ended in: ${Date.now() - start}ms`);
-        console.log('***');
+        console.log('================================');
       }),
     );
   }
